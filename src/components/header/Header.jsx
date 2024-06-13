6ce9184ab9e369af105a91cadbd1f197a1f9b421
jonbaidev <jonathanbaideveloper@gmail.com>
2024-06-13 18:13:34 -0300

    Criando componente Header, falta criacao dos inputs

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,40 @@
 import "./header.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBed, faCar, faGlobe, faPlane, faTaxi } from "@fortawesome/free-solid-svg-icons"
 
 const Header = () => {
   return (
-    <div className="Header">
+    <div className="header">
+        <div className="headerContainer">
+        <div className="headerList">
+            <div className="headerListItem active">
+                <FontAwesomeIcon icon={faBed} />
+                <span>Hospedagens</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faPlane} />
+                <span>Voos</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faCar} />
+                <span>Aluguel de Carros</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faGlobe} />
+                <span>Atrações</span>
+            </div>
+            <div className="headerListItem">
+                <FontAwesomeIcon icon={faTaxi} />
+                <span>Táxis (aeroporto)</span>
+            </div>
+        </div>
+
+        <h1 className="headerTitle">Encontre a sua próxima estadia.</h1>
+        <p className="headerDescription">
+            Encontre ofertas em hotéis, casas, apartamentos e muito mais...
+        </p>
+
+        </div>
         Header
     </div>
   )
